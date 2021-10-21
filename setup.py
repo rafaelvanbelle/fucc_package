95fc3a8d9c6e49a9eff80a355607ab55967de99b
rafael van belle <rafael.vanbelle@gmail.com>
2021-10-21 16:11:50 +0200

    increased version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fucc", 
-    version="0.0.3",
+    version="0.0.4",
     author="Rafael Van Belle",
     author_email="rafael@gmail.com",
     description="Fraudulent Use of Credit Card",
